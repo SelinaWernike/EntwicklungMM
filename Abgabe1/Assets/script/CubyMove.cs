63a3cb6be8811899d5885aef29824f187a2e3fdb
SelinaWernike <63050357+SelinaWernike@users.noreply.github.com>
2020-05-09 14:51:28 +0200

    Einbindung URP. Erste Versuche fuer Shader Prefab.

--- Abgabe1/Assets/script/CubyMove.cs
+++ Abgabe1/Assets/script/CubyMove.cs
@@ -7,7 +7,7 @@ public class CubyMove : MonoBehaviour
     private Vector3 move;
  
     private Quaternion rotate;
-    private Vector3 CamMove;
+    private float rotationAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +17,19 @@ public class CubyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotating();
-       float moveHorizontal = Input.GetAxis("Horizontal");
+       rotationAngle += Input.GetAxis("Horizontal") * Time.deltaTime;
        float moveVertical = Input.GetAxis("Vertical");
-        move = new Vector3(moveHorizontal,0,moveVertical);
+        rotating();
+        move = new Vector3(0,0,moveVertical);
     
         
-       transform.Translate(move * Time.deltaTime); 
+       transform.Translate(move * Time.deltaTime * 2f); 
     }
-    //Rotation des Würfels
+    //Rotation des Würfels. Anpassung der Rotation
     private void rotating() {
-        float moveCamHorizontal = Input.GetAxis("CamHorizontal");
-       float moveCamVertical = Input.GetAxis("CamVertical");
-       CamMove = new Vector3(moveCamHorizontal,0,moveCamVertical);
-       rotate = Quaternion.LookRotation(CamMove ,Vector3.up);
-        transform.rotation = rotate;
+        Vector3 playerDirection = new Vector3(Mathf.Sin(rotationAngle),0,Mathf.Cos(rotationAngle));
+        rotate = Quaternion.LookRotation(playerDirection, Vector3.up);
+        transform.rotation = rotate; 
     }
 
    
