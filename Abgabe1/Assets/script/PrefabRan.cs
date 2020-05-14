a10a3ce5536c9419384d7882075f57bc5c9a68de
SelinaWernike <Selina.Wernike@Student.HTW-Berlin.de>
2020-05-14 18:47:37 +0200

    Fertige Abgabe.

--- Abgabe1/Assets/script/PrefabRan.cs
+++ Abgabe1/Assets/script/PrefabRan.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PrefabRan : MonoBehaviour
 {
     public GameObject myPrefab;
+    public GameObject parentTarget;
     private float rotate;
     // Start is called before the first frame update
     //Erstellt zufällige Prefabs mit zufälligen Positionen zu und fügt manchen ein Script hinzu
     void Start()
     {
         
-        for(int i = 0; i <= Random.Range(1,10); i++) {
+        for(int i = 0; i <= Random.Range(1,5); i++) {
 
-       GameObject go = Instantiate(myPrefab,new Vector3(Random.Range(-2.0f,2.0f),0.1f,Random.Range(-2.0f,2.0f)), Quaternion.Euler(0,Random.Range(0,360),0)) as GameObject;
+       GameObject go = Instantiate(myPrefab,new Vector3(Random.Range(-3.0f,3.0f),0.1f,Random.Range(-2.0f,2.0f)), Quaternion.Euler(0,Random.Range(0,360),0)) as GameObject;
        if(Random.Range(0,4) <= 2.0f) {
+        go.transform.parent = parentTarget.transform;
        go.AddComponent<humanRot>(); 
        }
         
